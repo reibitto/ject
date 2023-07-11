cd2cc05e9dd87b6616fc728bd6800cd5e043947f
reibitto <reibitto@users.noreply.github.com>
2023-07-11 10:07:51 +0900

    Update dependencies

--- project/V.scala
+++ project/V.scala
@@ -1,19 +1,19 @@
 object V {
-  val enumeratum = "1.7.0"
+  val enumeratum = "1.7.2"
 
-  val jsoup = "1.15.2"
+  val jsoup = "1.16.1"
 
   val lucene = "8.11.2"
 
-  val scalaXml = "2.1.0"
+  val scalaXml = "2.2.0"
 
   val slf4j = "1.7.36"
 
   val slinky = "0.6.8"
 
-  val sttp = "3.7.1"
+  val sttp = "3.8.16"
 
-  val zio = "2.0.0"
+  val zio = "2.0.15"
 
-  val zioProcess = "0.7.1"
+  val zioProcess = "0.7.2"
 }
